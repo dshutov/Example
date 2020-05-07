9957269a057e4ecdc417449a05449a58b9bafac5
vipmedukvie <vipmedukvie@gmail.com>
2020-05-07 12:39:35 +0400

    optimize imports

--- src/main/java/tools/driver/WebDriverSuppliers.java
+++ src/main/java/tools/driver/WebDriverSuppliers.java
@@ -2,14 +2,8 @@ package tools.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import tools.properties.PropertiesConfig;
-import tools.properties.PropertiesKeys;
-
-import java.io.File;
 
 class WebDriverSuppliers {
 
